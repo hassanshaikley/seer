f66ff0854ee50ea0e3cd1681ae226eef34a70f3f
Hassan Shaikley <hassan.shaikley@gmail.com>
2022-11-05 16:01:55 -0400

    Returns erroneous paths in order

--- lib/seer.ex
+++ lib/seer.ex
@@ -1,18 +1,34 @@
 defmodule Seer do
-  @moduledoc """
-  Documentation for `Seer`.
-  """
+  import Plug.Conn
 
-  @doc """
-  Hello world.
+  @locales ["en", "fr", "de"]
 
-  ## Examples
+  def init(default), do: default
 
-      iex> Seer.hello()
-      :world
+  # def call(%Plug.Conn{params: %{"locale" => loc}} = conn, _default) when loc in @locales do
+  #   assign(conn, :locale, loc)
+  # end
 
-  """
-  def hello do
-    :world
+  def call(conn, default) do
+    target_path = conn.request_path
+
+    project =
+      Mix.Project.config()[:app]
+      |> Atom.to_string()
+      |> Kernel.<>("Web")
+      |> Phoenix.Naming.camelize()
+      |> String.to_atom()
+      |> Module.concat(Router)
+
+    routes = project.__routes__()
+
+    routes =
+      Enum.sort(routes, fn a, b ->
+        String.jaro_distance(a.path, target_path) > String.jaro_distance(b.path, target_path)
+      end)
+
+    paths = Enum.map(routes, fn x -> x.path end)
+
+    assign(conn, :seer, %{paths: paths})
   end
 end
